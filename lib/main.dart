4b1f4ccd66e3081359140627f0e5f08c8a957dc1
Kipkorir-Gideon <gidaliosventures@gmail.com>
2023-01-27 15:51:11 +0300

    Use Scaffold widget to improve the UI.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is My default text'),
+      ),
     );
   }
 }
